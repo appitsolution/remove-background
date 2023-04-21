2648efdff6338cdab4b819cd32b9a4deeb2ce86a
appitsolution <applicationitsolution@gmail.com>
2023-04-21 10:49:49 +0300

    added fix

--- js/remove-bg.js
+++ js/remove-bg.js
@@ -3,52 +3,98 @@ const fileInput1 = document.querySelector("#upload-file-1");
 const fileInput2 = document.querySelector("#upload-file-2");
 const downloadLink = document.querySelector(".hero__content-upload-download");
 const errorMessage = document.querySelector(".hero__content-upload-error");
+const loaderCurrent = document.querySelector('.loader-current')
+const loaderWrap = document.querySelector('.loader-wrap')
+
+let scoreCurrent = 0
+const score = document.querySelector("#loader-score")
+
+
+const startUpload = () => {
+
+  const timerScore = setInterval(() => {
+  
+      scoreCurrent += 1
+      score.textContent = `${scoreCurrent}%`
+  
+      if (scoreCurrent >= 100) {
+          clearInterval(timerScore)
+  
+          loaderCurrent.classList.add('active')
+          loaderWrap.classList.add('active')
+          return
+      }
+  }, 100)
+}
 
 
 
 const removeBackground = (event) => {
   event.preventDefault();
-  if(event.target.files.length === 0) return
-  
+  if (event.target.files.length === 0) return;
+
   const file = event.target.files[0];
-  if(file.hasOwnProperty('name') ) return;
-  if (file.name.slice(-4,file.name.length) !== ".jpg") {
-    console.dir(file.name)
+  if (file.hasOwnProperty("name")) return;
+  if (file.name.slice(-4, file.name.length) !== ".jpg") {
     errorMessage.classList.add("active");
     setTimeout(() => {
-        errorMessage.classList.remove('active')
+      errorMessage.classList.remove("active");
     }, 10000);
     return;
   }
+  startUpload()
+  scoreCurrent = 0;
+  if(document.querySelector('.loader-current').classList.contains('active')){
+    console.log(loaderCurrent.classList)
+    loaderCurrent.classList.remove('active')
+    loaderWrap.classList.remove('active')
+  }
+
 
   const reader = new FileReader();
-
+document.querySelector('.loader-body').classList.add('active')
   reader.addEventListener("load", () => {
     const formData = new FormData();
     formData.append("image_file", file);
     formData.append("size", "auto");
 
-    fetch("https://api.remove.bg/v1.0/removebg", {
-      method: "POST",
-      headers: {
-        "X-Api-Key": apiKey,
-      },
-      body: formData,
-    })
-      .then((response) => response.blob())
-      .then((result) => {
-        const url = URL.createObjectURL(result);
+    const xhr = new XMLHttpRequest();
+    
+    xhr.upload.addEventListener("progress", function (event) {
+      if (event.lengthComputable) {
+        const percentComplete = (event.loaded / event.total) * 100;
+        scoreCurrent = percentComplete
+        console.log(`Загрузка... ${percentComplete.toFixed(2)}%`);
+      }
+    });
+
+    xhr.addEventListener("load", function () {
+      if (xhr.status === 200) {
+        const url = URL.createObjectURL(xhr.response);
 
         downloadLink.href = url;
         downloadLink.download = "result.png";
         downloadLink.style.display = "block";
         downloadLink.click();
-      })
-      .catch((error) => console.error(error));
+      } else {
+        console.error(xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener("error", function () {
+      console.error("Network Error");
+    });
+
+    xhr.open("POST", "https://api.remove.bg/v1.0/removebg");
+    xhr.setRequestHeader("X-Api-Key", apiKey);
+    xhr.responseType = "blob";
+    xhr.send(formData);
   });
 
   reader.readAsDataURL(file);
-}
+};
+
+
 
 
 
